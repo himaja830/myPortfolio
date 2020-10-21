8090fdf2658a82e6c6c117588b5c4b411ab4b10a
Himaja830 <Himaja.Sriramaneni@gmail.com>
2020-10-20 22:39:11 -0500

    Remove debug router

--- src/App.js
+++ src/App.js
@@ -8,27 +8,11 @@ import Portfolio from "./components/Portfolio";
 import Contact from "./components/Contact";
 import About from "./components/About ";
 
-class DebugRouter extends BrowserRouter {
-  constructor(props) {
-    super(props);
-    console.log("initial history is: ", JSON.stringify(this.history, null, 2));
-    this.history.listen((location, action) => {
-      console.log(
-        `The current URL is ${location.pathname}${location.search}${location.hash}`
-      );
-      console.log(
-        `The last navigation action was ${action}`,
-        JSON.stringify(this.history, null, 2)
-      );
-    });
-  }
-}
-
 function App() {
   return (
     <>
       <CssBaseline />
-      <DebugRouter basename="/myPortfolio">
+      <BrowserRouter basename="/myPortfolio">
         <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/resume" component={Resume} />
@@ -36,7 +20,7 @@ function App() {
           <Route path="/contact" component={Contact} />
           <Route path="/about" component={About} />
         </Switch>
-      </DebugRouter>
+      </BrowserRouter>
     </>
   );
 }
