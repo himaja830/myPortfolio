9abb8d5a0338bd17a90fe7f62f951f023d480e8a
Himaja830 <Himaja.Sriramaneni@gmail.com>
2020-10-20 22:12:06 -0500

    Add BrowserRouter

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import "./App.css";
 import Home from "./components/index";
-import { Route } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import Resume from "./components/Resume";
 import Portfolio from "./components/Portfolio";
 import Contact from "./components/Contact";
@@ -12,11 +12,13 @@ function App() {
   return (
     <>
       <CssBaseline />
-      <Route exact path="/" component={Home} />
-      <Route path="/resume" component={Resume} />
-      <Route path="/portfolio" component={Portfolio} />
-      <Route path="/contact" component={Contact} />
-      <Route path="/about" component={About} />
+      <BrowserRouter basename="/test">
+        <Route exact path="/" component={Home} />
+        <Route path="/resume" component={Resume} />
+        <Route path="/portfolio" component={Portfolio} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/about" component={About} />
+      </BrowserRouter>
     </>
   );
 }
