b97a924d039fa97a0e39c90fcb769068357d97c8
Himaja830 <Himaja.Sriramaneni@gmail.com>
2020-10-20 22:23:23 -0500

    Add debug router to print logs

--- src/App.js
+++ src/App.js
@@ -8,6 +8,22 @@ import Portfolio from "./components/Portfolio";
 import Contact from "./components/Contact";
 import About from "./components/About ";
 
+class DebugRouter extends BrowserRouter {
+  constructor(props) {
+    super(props);
+    console.log("initial history is: ", JSON.stringify(this.history, null, 2));
+    this.history.listen((location, action) => {
+      console.log(
+        `The current URL is ${location.pathname}${location.search}${location.hash}`
+      );
+      console.log(
+        `The last navigation action was ${action}`,
+        JSON.stringify(this.history, null, 2)
+      );
+    });
+  }
+}
+
 function App() {
   return (
     <>
