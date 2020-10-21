4e8b85aada232c9c292743aa579843d5a18b4bd8
Himaja830 <Himaja.Sriramaneni@gmail.com>
2020-10-20 22:25:13 -0500

    Add Switch

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import "./App.css";
 import Home from "./components/index";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Resume from "./components/Resume";
 import Portfolio from "./components/Portfolio";
 import Contact from "./components/Contact";
@@ -28,13 +28,15 @@ function App() {
   return (
     <>
       <CssBaseline />
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
-        <Route exact path="/" component={Home} />
-        <Route path="/resume" component={Resume} />
-        <Route path="/portfolio" component={Portfolio} />
-        <Route path="/contact" component={Contact} />
-        <Route path="/about" component={About} />
-      </BrowserRouter>
+      <DebugRouter basename={process.env.PUBLIC_URL}>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/resume" component={Resume} />
+          <Route path="/portfolio" component={Portfolio} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/about" component={About} />
+        </Switch>
+      </DebugRouter>
     </>
   );
 }
